19188a76bb624484a3e6a8170946b5e89989a79e
Anthony Howell <anthony@pulcer.net>
2024-03-07 22:50:52 -0800

    Potential design change

--- Product Manager 5/InitiativeView.swift
+++ Product Manager 5/InitiativeView.swift
@@ -1,26 +1,29 @@
 import SwiftUI
 
 struct InitiativeView: View {
+
     let initiative: Initiative
-    
+
     var body: some View {
         let containsUrgentTask = initiative.tasks?.contains(where: { $0.isUrgent && !$0.isCompleted }) ?? false
-
         let uncompletedTasksCount = initiative.tasks?.filter { !$0.isCompleted }.count ?? 0
-        
+
         VStack(alignment: .leading) {
             Text(initiative.title).font(.title2)
-            
-            HStack{
+
+            HStack {
                 if let notes = initiative.notes, !notes.isEmpty {
                     Text("Notes: \(notes.count)").font(.headline)
                 }
-                
+
                 Text("Tasks: \(uncompletedTasksCount)").font(.headline)
             }
         }
         .padding(8)
-        .background(containsUrgentTask ? Color.red.opacity(0.8) : Color.blue.opacity(0.4))
+        .background(
+            LinearGradient(gradient: Gradient(colors: [containsUrgentTask ? Color.pink : Color.blue, containsUrgentTask ? Color.red : Color.cyan]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .opacity(0.6)
+        )
         .cornerRadius(8)
     }
 }
